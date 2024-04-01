56c059d54079746b7ffb18be717d29321438f546
santiago <santiagonahuellopez@gmail.com>
2024-04-01 02:49:20 -0300

    feat: Add of clear button and board update.
    Add of a clrear button, turn controller and board update function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 import { Square } from './components/Square/Square';
 
-const board = Array(9).fill(null);
-
+const TURNS = {
+  X: 'X',
+  O: 'O'
+};
 export const App = () => {
+  const initialBoardState = Array(9).fill(null);
+  const [board, setBoard] = useState<(string | null)[]>(initialBoardState);
+  const [turn, setTurn] = useState<string>(TURNS.X);
+
+  const updateBoard = (index: number) => {
+    if (board[index]) return;
+
+    const auxBoard = [...board];
+    auxBoard[index] = turn;
+    setBoard(auxBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
+
+  const clearBoard = () => {
+    setBoard(initialBoardState);
+    setTurn(TURNS.X);
+  };
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <section className='game'>
         {board.map((_, index) => (
-          <Square key={`square-${index}`}>{index}</Square>
+          <Square key={`square-${index}`} updateBoard={updateBoard} index={index}>
+            {board[index]}
+          </Square>
         ))}
       </section>
+      <section>
+        <span>Turn: {turn}</span>
+      </section>
+      <footer>
+        <button onClick={clearBoard}>Limpiar tablero</button>
+      </footer>
     </main>
   );
 };
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,4 +1,20 @@
+import { ReactNode } from 'react';
 import './square.css';
-export const Square = ({ children }) => {
-  return <div className='square'>{children}</div>;
+
+interface SquareProps {
+  children: ReactNode;
+  updateBoard: (index: number) => void;
+  /* updateTurn: () => void; */
+  index: number;
+}
+export const Square = ({ children, updateBoard, index }: SquareProps) => {
+  const handleClick = () => {
+    updateBoard(index);
+    /* updateTurn(); */
+  };
+  return (
+    <div className='square' onClick={handleClick}>
+      {children}
+    </div>
+  );
 };
